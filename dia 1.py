c5d74531ef19a4407df88e7f5b95b69edea31f56
Leo <132487524+ledumape@users.noreply.github.com>
2024-03-13 10:55:52 -0300

    Add files via upload

---
+++ dia 1.py
@@ -0,0 +1,4 @@
+print("bem vindo ao gerador de nome para bandas .")
+cidade = input("qual o nome que voce nasceu?\n")
+pet = input("qual o nome do seu pet?\n")
+print("Your band name could be " + cidade + " " + pet)
